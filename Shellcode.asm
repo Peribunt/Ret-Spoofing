92d8859fc68b83c492f73369b201d0ab74ab1a7b
ReaP <67383323+Peribunt@users.noreply.github.com>
2022-07-04 20:06:12 +0200

    Update Shellcode.asm

--- Shellcode.asm
+++ Shellcode.asm
@@ -1,15 +1,17 @@
 .code
 
 InitSpoofCall PROC
-mov rsi, rcx
-mov rdi, rdx
-ret
+  movq xmm15, rdx
+  pinsrq xmm15, rcx, 1
+  ret
 InitSpoofCall ENDP
 
 SpoofCall PROC
-mov rbx, qword ptr[ rsp ]
-mov qword ptr[ rsp ], rdi
-jmp rsi
+	mov rbx, qword ptr[rsp]
+	movq qword ptr[rsp], xmm15
+	movhlps xmm15, xmm15
+	movq rsi, xmm15
+	jmp rsi
 SpoofCall ENDP
 
 END
